f6f3e6c5bdfcf9e289ad31645c2c61d9515ff3eb
momokappp3 <momokappp3@gmail.com>
2022-07-02 22:57:49 +0900

    枠追加光らせる

---
+++ Assets/Script/ProjectMemo.cs
@@ -0,0 +1,30 @@
+/*
+ * 
+ *  素材
+ *  フレームデザイン　Twitterで宣伝　20個まで無料 加工OK
+ *  
+ *  1 https://frames-design.com/2015/01/02/f15051/
+ *  2 https://frames-design.com/2015/01/02/f15061/
+ *  3 https://frames-design.com/2015/01/01/f05601/
+ *  4 https://frames-design.com/2015/01/02/f15251/
+ *  5 https://frames-design.com/2015/01/02/f15021/
+ *  
+ *  
+ *  //Spritを光らせる MIT License
+ *  Git https://github.com/Elringus/SpriteGlow
+ * 
+ *  3Dモデル
+ *  crown MIT License 改変済
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
